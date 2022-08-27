df32b133eb200efd09d99ee841ef586c8ea1b745
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-27 16:01:00 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    bodyBC:document.querySelector('body'),    
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+};
+ 
+refs.buttonStart.addEventListener('click', onStart);
+refs.buttonStop.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+let timerId = null;
+
+function onChangeBackColor() {
+  const startChangeColor = getRandomHexColor();
+   refs.bodyBC.style.backgroundColor = startChangeColor;
+}
+function onStart() {
+   
+  timerId = setInterval(() => {
+   onChangeBackColor()
+  }, 1000);
+  refs.buttonStart.setAttribute("disabled", true);
+  
+};
+
+function onStop() { 
+  clearInterval(timerId); 
+  
+  refs.buttonStart.removeAttribute("disabled", false);
+
+};
 
